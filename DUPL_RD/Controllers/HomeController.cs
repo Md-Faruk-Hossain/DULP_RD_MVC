954794dded5806bf52a98d1ca7d4aa7c6f4d02e6
Md Faruk Hossain <mfarukhossain63@gmail.com>
2023-05-13 02:01:25 -0500

    first commit

---
+++ DUPL_RD/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using DUPL_RD.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DUPL_RD.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookDbContext db = new BookDbContext();
+
+        public ActionResult BookList()
+        {
+            return View(db.Books.ToList());
+        }
+        public ActionResult exportReport()
+        {
+            ReportDocument reportDocument = new ReportDocument();
+            reportDocument.Load(Path.Combine(Server.MapPath("~/Report"), "BooksCrystalReport.rpt"));
+            reportDocument.SetDataSource(db.Books.ToList());
+            Response.Buffer = false;
+            Response.ClearContent();
+            Response.ClearHeaders();
+            try
+            {
+                Stream stream = reportDocument.ExportToStream(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat);
+                return File(stream, "application/pdf");
+            }
+            catch
+            {
+
+                throw;
+            }
+        }
+        //
+       
+        
+        private List<Book> books = new List<Book>
+        {
+            // Populate your events data here
+        };
+
+        public ActionResult Index()
+        {
+            return View(books);
+        }
+
+        [HttpPost]
+        public ActionResult Search(DateTime startDate, DateTime endDate)
+        {
+            var results = books.Where(e => e.Date >= startDate && e.Date <= endDate).ToList();
+            return PartialView("_BookList", results);
+        }
+        
+
+        //
+        //public IEnumerable<Book> results { get; set; }
+        //public void OnGet()
+        //{
+        //    results=db.Books.ToList();
+        //}
+        //public void OnPost(DateTime startdate, DateTime enddate)
+        //{
+        //    results=(from x in db.Books where(x.Date<= startdate)&&(x.Date>=enddate)select x).ToList();
+        //}
+
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
